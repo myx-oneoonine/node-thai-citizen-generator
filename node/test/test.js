960fe999fedc93ce460ec7931715b021601b1e36
Phanuphat Cholatda <remix.tk108@gmail.com>
2017-07-24 23:48:02 +0700

    add test class

---
+++ node/test/test.js
@@ -0,0 +1,12 @@
+var thGen = require('../generator/core.js')
+
+let assert = require('assert')
+
+
+if (!global.is_checking) {
+
+
+    // assert.equal(thaiCitizenValidator('1239900124231'), true, "1. best case 01")
+  
+    console.log('Passed')
+}
